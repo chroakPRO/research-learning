4688ddf8d4d6d58fb42b12c7dc081f21477a5bab
coopersec <Christophereek97@gmail.com>
2022-04-08 02:15:11 +0200

    123

---
+++ Research/game-cheats/api-test/CHEKAPIClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MalShare.NET
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MSClient client = new MSClient("fee963fbeef39a63af94d268550a87abfe29366507d6f7a53aaa37c7afe9871c");
+
+            Console.WriteLine(client.GetHashList());
+
+        }
+    }
+}
